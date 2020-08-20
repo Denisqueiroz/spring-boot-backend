7257db57271fc6b5854bc3b1532a7cf336cc79c1
unknown <denisqueiroz1@gmail.com>
2020-08-20 09:43:27 -0300

    Ajuste no endpoint /categorias/{id}

--- src/main/java/com/queiroz/cursomc/services/CategoriaService.java
+++ src/main/java/com/queiroz/cursomc/services/CategoriaService.java
@@ -2,12 +2,12 @@ package com.queiroz.cursomc.services;
 
 import java.util.Optional;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.queiroz.cursomc.domain.Categoria;
 import com.queiroz.cursomc.repositories.CategoriaRepository;
+import com.queiroz.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,6 +17,7 @@ public class CategoriaService {
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
